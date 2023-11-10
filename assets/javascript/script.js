bfab3fe5d8c4a6dc1120ab318b71842ed0255a5d
Echo914 <fortuneklabi6@gmail.com>
2023-11-10 15:09:57 +0000

    commit templates: Services, About, and Contact

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,6 +8,27 @@ function hide_overlay() {
     overlay.style.right = "100%";
 }
 
+function animate_welcome_message() {
+    var msg_box = document.getElementById("welcome-message");
+    var w1 = document.getElementById("w1");
+    var w2 = document.getElementById("w2");
+    if (msg_box.style.backgroundColor === "rgba(0, 0, 0, 0.9)") {
+        w2.style.transform = "translateX(-100%)"
+        w2.style.right = "100%";
+        w1.style.opacity = "0";
+        msg_box.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    } else {
+        msg_box.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+        setTimeout(()=>{
+            w1.style.opacity = "1";
+            setTimeout(()=>{
+                w2.style.right = "92%";
+                w2.style.transform = "translateX(100%)";
+            }, 2000);
+        }, 3000);
+    }
+}
+
 var navbar = document.querySelector("#nav");
 var header = document.querySelector(".header-link"); 
 
@@ -18,3 +39,9 @@ setInterval(function() {
         navbar.classList.add("hidden");
     }
 }, 50);
+
+// Animates the welcome message on homepage
+animate_welcome_message();
+setInterval(animate_welcome_message, 12000);
+
+var fade_command = "fade-in";
